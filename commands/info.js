7a0b8184a04d775d3aeaa59bf3442490121ae10d
Luiz Eduardo Gotardo <59850361+DoginUwU@users.noreply.github.com>
2020-08-18 14:22:30 -0300

    .

--- commands/info.js
+++ commands/info.js
@@ -33,7 +33,7 @@ exports.run = (client, message, args, language) => {
   .addField(lg[language].commands_, cmds, true)
   .addField("Ping: ⚡️", `${lg[language].latency2} ${Date.now() - message.createdTimestamp}ms\n${lg[language].api_latency2} ${Math.round(client.ping)}ms`, true)
   .addField(lg[language].library, `<:discordicon:577291169067696128> \*\*[Discord.js ${pack.dependencies["discord.js"]}](https://discord.js.org)\*\* \n <:weathericon:577293163975409694> \*\*[Weather-js ${pack.dependencies["weather-js"]}](https://www.npmjs.com)\*\* \n <:icons8playbutton96:577293769724919818> \*\*[Youtube-api ${pack.dependencies["simple-youtube-api"]}](https://www.youtube.com)\*\* `, false)
-  .addField("Links: 🔗", `**[Website](https://nothing.com)** • **[Add to your server](https://discordapp.com/oauth2/authorize?=&client_id=568539957703213077&scope=bot&permissions=8)** • **[Join on server](https://discord.gg/XXX)**`, false)
+  .addField("Links: 🔗", `**[Website](https://nothing.com)** • **[Add to your server](https://discordapp.com/oauth2/authorize?=&client_id=568539957703213077&scope=bot&permissions=8)** • **[Join on server](https://discord.gg/aaaaaaaaaaaaaaaasdsad)**`, false)
   .setFooter(`${lg[language].resquest_command} ${message.author.tag}`, `${message.author.avatarURL}`)
   
   
