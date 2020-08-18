294bb4ec3ac536d8b54c331782316de99f625010
Luiz Eduardo Gotardo <59850361+DoginUwU@users.noreply.github.com>
2020-08-18 14:00:08 -0300

    .

--- commands/invite.js
+++
@@ -1,7 +0,0 @@
-const Discord = require('discord.js');
-const Config = require('../config.json')
-const DC = Config.DC;
-
-exports.run = (client, message, args) => {
-  message.reply(`${Config.IL}`);
-}
